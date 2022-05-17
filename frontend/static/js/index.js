3362254bba1d03bbca58ca52ae9dfd3861f12d01
Juan Camilo <jccifuentes21@gmail.com>
2022-05-17 10:15:08 -0700

    added how to retrieve the data from the countries array

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -2,4 +2,17 @@ import {getCountries} from './countries.js'
 
 const countries = getCountries();
 
-console.log(countries);
+// countries.map(element =>{
+//   console.log(element.name.common)
+//   console.log(element.population)
+//   console.log(element.region)
+//   if(element.capital != null){
+//     element.capital.map(capitals => {
+//       console.log(capitals)
+//     })
+//   } else {
+//     console.log("the country does not have a capital")
+//   }
+// })
+ 
+// console.log(countries);
