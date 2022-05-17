9fb55fede4bd4025c23f8e45098bb9c1ef3ae13d
DaweUrbi <urban.da@icloud.com>
2022-05-16 20:24:51 -0700

    HTML part

--- frontend/static/js/views/Homepage.js
+++ frontend/static/js/views/Homepage.js
@@ -7,6 +7,40 @@ export default class extends AbstractView{
     }
 
     async getHTML() {
-        return "<h1>Homepage</h1>";
+        // const div = document.createElement("div");
+        // const section = document.createElement("section")
+        
+        return `
+            <section id=header>
+                <div id="header">
+                    <h1>Where in the world?</h1>
+                    <button class="mode">Dark Mode</button>
+                </div>
+            </section>
+
+            <section id=homepage>
+                <div id="background_image">
+                    <div id=rectangle>
+                        <div id=country></div>
+                    </div>
+                    <button id="explore">Explore</button>    
+                </div>
+            </section>
+
+            <section id="footer">
+                <h3>Final project of CICCC students</h3>
+            </section>
+
+
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        `;
     }
 }
