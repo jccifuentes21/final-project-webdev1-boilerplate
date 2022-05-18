43a59f3db5d564fe5d17c9b8d7defca1cd203673
Juan Camilo <jccifuentes21@gmail.com>
2022-05-18 09:39:46 -0700

    updated work

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -14,5 +14,48 @@ const countries = getCountries();
 //     console.log("the country does not have a capital")
 //   }
 // })
+
+let countryObject = []
+
+
+const countryArr = [];
+
+countries.map(country => {
+  countryObject = {
+    cca3: null,
+    name: null,
+    nativeName: null,
+    population: null,
+    region: null,
+    subRegion: null,
+    capital: null,
+    domain: null,
+    currencies: [],
+    languages: [],
+    borderCountries: []
+  }
+
+  countryObject.cca3 = country.cca3;
+  countryObject.name = country.name.common;
+  // if(country.name.nativeName[0])
+  countryObject.nativeName = country.name.nativeName
+  countryObject.population = country.population;
+  countryObject.region = country.region;
+  countryObject.subRegion = country.subregion;
+  if(country.capital != null) countryObject.capital = country.capital[0];
+  else countryObject.capital = "This country does not have a capital"
+
+  if(country.tld != null) countryObject.domain = country.tld[0];
+  else countryObject.domain = "This country does not have a domain"
+  // countryObject.currencies = country.currencies
+  // countryObject.languages = country.languages
+  // countryObject.borderCountries = country.borders
+
+  countryArr.push(countryObject);
+  console.log(countryObject)
+
+})
+
+console.log(countryArr)
  
-// console.log(countries);
+console.log(countries);
