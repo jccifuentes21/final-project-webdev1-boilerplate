8870119f9c0e5a46dde8dc5e4b4e796278fd4f60
Juan Camilo <jccifuentes21@gmail.com>
2022-05-18 10:20:36 -0700

    created array of countries with useful info

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -2,19 +2,6 @@ import {getCountries} from './countries.js'
 
 const countries = getCountries();
 
-// countries.map(element =>{
-//   console.log(element.name.common)
-//   console.log(element.population)
-//   console.log(element.region)
-//   if(element.capital != null){
-//     element.capital.map(capitals => {
-//       console.log(capitals)
-//     })
-//   } else {
-//     console.log("the country does not have a capital")
-//   }
-// })
-
 let countryObject = []
 
 
@@ -27,35 +14,40 @@ countries.map(country => {
     nativeName: null,
     population: null,
     region: null,
-    subRegion: null,
+    subregion: null,
     capital: null,
     domain: null,
     currencies: [],
     languages: [],
-    borderCountries: []
+    borderCountries: [],
+    flagURL: null
   }
 
   countryObject.cca3 = country.cca3;
   countryObject.name = country.name.common;
-  // if(country.name.nativeName[0])
-  countryObject.nativeName = country.name.nativeName
+
+  if(country.name.nativeName != null) countryObject.nativeName = (Object.values(country.name.nativeName)[0]).common
+  else countryObject.nativeName = "This country does not have a native name"
+
   countryObject.population = country.population;
   countryObject.region = country.region;
-  countryObject.subRegion = country.subregion;
+
+  if (country.subregion != null) countryObject.subregion = country.subregion;
+  else countryObject.subregion = "This country does not have a sub region"
+
   if(country.capital != null) countryObject.capital = country.capital[0];
   else countryObject.capital = "This country does not have a capital"
 
   if(country.tld != null) countryObject.domain = country.tld[0];
   else countryObject.domain = "This country does not have a domain"
-  // countryObject.currencies = country.currencies
-  // countryObject.languages = country.languages
-  // countryObject.borderCountries = country.borders
+
+  countryObject.currencies = country.currencies
+  countryObject.languages = country.languages
+  countryObject.borderCountries = country.borders
+  countryObject.flagURL = country.flags.png
 
   countryArr.push(countryObject);
-  console.log(countryObject)
 
 })
 
-console.log(countryArr)
- 
-console.log(countries);
+console.log(countryArr);
